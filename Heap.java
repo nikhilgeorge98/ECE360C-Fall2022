d18c6b7214f533762304baa053e9fbfe592c6a02
Nikhil George <nikhil.george98@gmail.com>
2022-10-24 18:57:51 -0500

    Programming Assignment 2 submitted version - passed all test cases

---
+++ Heap.java
@@ -0,0 +1,230 @@
+/*
+ * Name: Nikhil George
+ * EID: ng25762
+ */
+
+// Implement your heap here
+// Private methods may be added to this file, but don't remove anything
+// Include this file in your final submission
+
+import java.util.ArrayList;
+
+public class Heap implements HeapInterface {
+    private ArrayList<City> minHeap;
+
+    public Heap() {
+        minHeap = new ArrayList<City>();
+    }
+
+    /**
+     * buildHeap(ArrayList<City> cities)
+     * Given an ArrayList of Cities, build a min-heap keyed on each City's minDist
+     * Time Complexity - O(nlog(n)) or O(n)
+     * 
+     * Should assign cities to the minHeap instance variable
+     *
+     *
+     * @param cities
+     */
+    public void buildHeap(ArrayList<City> cities) {
+        minHeap = cities;
+        // TODO: implement this method
+        for(int i = 0; i<cities.size(); i++){
+            // System.out.println("Currently inserting "+cities.get(i).getName()+" at index "+i);
+            this.minHeap.set(i, cities.get(i));
+            heapifyUp(cities.get(i), i);
+            // System.out.println();
+        }
+        // for(int i = 0; i<cities.size(); i++){
+        //     System.out.println("Currently inserting "+cities.get(i).getName()+" at index "+i);
+        //     insertNode(cities.get(i));
+        //     System.out.println();
+        // }
+        // System.out.println(this.minHeap.toString());
+        // String output = "";
+        // for (int i = 0; i < minHeap.size(); i++) {
+        //     output += minHeap.get(i).getName() + " ";
+        // }
+    }
+
+    /**
+     * insertNode(City in)
+     * Insert a City into the heap.
+     * Time Complexity - O(log(n))
+     *
+     * @param in - the City to insert.
+     */
+    public void insertNode(City in) {
+        // TODO: implement this method
+        this.minHeap.add(in);
+        int index = this.minHeap.size()-1;
+        // System.out.println("Inserted "+in.getName()+" with weight "+in.getMinDist()+" at index "+index);
+        heapifyUp(in, index);
+    }
+
+    /**
+     * findMin()
+     * Time Complexity - O(1)
+     *
+     * @return the minimum element of the heap.
+     */
+    public City findMin() {
+        // TODO: implement this method
+        return this.minHeap.get(0);
+    }
+
+    /**
+     * extractMin()
+     * Time Complexity - O(log(n))
+     *
+     * @return the minimum element of the heap, AND removes the element from said heap.
+     */
+    public City extractMin() {
+        // TODO: implement this method
+        City min = findMin();
+        delete(0);
+        return min;
+    }
+
+    /**
+     * delete(int index)
+     * Deletes an element in the min-heap given an index to delete at.
+     * Time Complexity - O(log(n))
+     *
+     * @param index - the index of the item to be deleted in the min-heap.
+     */
+    public void delete(int index) {
+        // TODO: implement this method
+        int lastIndex = this.minHeap.size()-1;
+        City toDelete = this.minHeap.get(index);
+        City lastCity = this.minHeap.get(lastIndex);
+
+        // System.out.println("Initial heap");
+        // for(City x: this.minHeap){
+        //     System.out.println(x.getName() +" "+ x.getIndex() +" "+ x.getMinDist());
+        // }
+
+        this.minHeap.set(index, lastCity);
+        this.minHeap.set(lastIndex, toDelete);
+        this.minHeap.remove(lastIndex);
+        // System.out.println("After deleting, before heapify");
+        // for(City x: this.minHeap){
+        //     System.out.println(x.getName() +" "+ x.getIndex() +" "+ x.getMinDist());
+        // }
+
+        heapifyUp(lastCity, index);
+        // System.out.println("Finished Heapify Up");
+        heapifyDown(lastCity, index);
+        // System.out.println("Finished Heapify Down");
+        // for(City x: this.minHeap){
+        //     System.out.println(x.getName() +" "+ x.getIndex() +" "+ x.getMinDist());
+        // }
+    }
+
+    /**
+     * changeKey(City r, int newDist)
+     * Changes minDist of City s to newDist and updates the heap.
+     * Time Complexity - O(log(n))
+     *
+     * @param r       - the City in the heap that needs to be updated.
+     * @param newDist - the new cost of City r in the heap (note that the heap is keyed on the values of minDist)
+     */
+    public void changeKey(City r, int newDist) {
+        // TODO: implement this method
+        // System.out.println("Current key of city "+ r.getName()+" at index "+ r.getIndex() + " is " + r.getMinDist());
+        this.minHeap.get(r.getIndex()).setMinDist(newDist);
+        // System.out.println("Changed key of city "+ r.getName()+" at index "+ r.getIndex() + " is " + r.getMinDist());
+        heapifyUp(this.minHeap.get(r.getIndex()), r.getIndex());
+        // System.out.println("Finished Heapify Up");
+        heapifyDown(this.minHeap.get(r.getIndex()), r.getIndex());
+        // System.out.println("Finished Heapify Down");
+        // for(City x: this.minHeap){
+        //     System.out.println(x.getName() +" "+ x.getIndex() +" "+ x.getMinDist());
+        // }
+
+    }
+
+    public String toString() {
+        String output = "";
+        for (int i = 0; i < minHeap.size(); i++) {
+            output += minHeap.get(i).getName() + " ";
+        }
+        return output;
+    }
+
+    private void heapifyUp(City city, int index){
+        // System.out.println("Inside Heapify Up");
+        int j;
+        if(index>0){
+            // System.out.println("More than 1 element so going into condition");
+            j = (int)Math.floor((index-1)/2);
+            // System.out.println("Parent of city "+ city.getName() + " at index " + index + " is city " + this.minHeap.get(j).getName()+ " at index " + j);
+            // System.out.println("Weight of city "+ city.getName() + " is " + city.getMinDist() + " and weight of parent " + this.minHeap.get(j).getName()+ " is " + this.minHeap.get(j).getMinDist());
+            if(city.getMinDist() < this.minHeap.get(j).getMinDist() || ((city.getMinDist() == this.minHeap.get(j).getMinDist()) && (city.getName() < this.minHeap.get(j).getName()))){
+                // System.out.println("Need to swap");
+                City tempCity = this.minHeap.get(j);
+
+                tempCity.setIndex(index);
+                city.setIndex(j);
+
+                this.minHeap.set(j, city);
+                this.minHeap.set(index, tempCity);
+                heapifyUp(city, j);
+            }
+        }
+        else{
+            city.setIndex(0);
+        }
+        // return null;
+    }
+
+    private void heapifyDown(City city, int index){
+        int n = minHeap.size();
+        // System.out.println("Inside Heapify Down");
+        // System.out.println("Size heap "+n);
+        // System.out.println("City: " + city.getName() + " weight: "+city.getMinDist());
+        int j;
+        int left, right;
+        if(2*index + 1 >= n){
+            // System.out.println("No children");
+            return;
+        }
+        else if(2*index + 1 < n-1){
+            left = 2*index + 1;
+            right = 2*index + 2;
+            // System.out.println("Left child: " + this.minHeap.get(left).getName() + " weight: "+this.minHeap.get(left).getMinDist());
+            // System.out.println("Right child: " + this.minHeap.get(right).getName() + " weight: "+this.minHeap.get(right).getMinDist());
+            if(this.minHeap.get(left).getMinDist() < this.minHeap.get(right).getMinDist() || ((this.minHeap.get(left).getMinDist() == this.minHeap.get(right).getMinDist()) && (this.minHeap.get(left).getName() < this.minHeap.get(right).getName()))){
+                j = left;
+            }
+            else{   
+                j = right;
+            }
+        }
+        else{
+            j = 2*index + 1;
+        }
+        if(this.minHeap.get(j).getMinDist() < city.getMinDist() || ((this.minHeap.get(j).getMinDist() == city.getMinDist()) && (this.minHeap.get(j).getName() < city.getName()))){
+            // System.out.println("Need to swap with city: "+this.minHeap.get(j).getName()+" at index "+j);
+            City tempCity = this.minHeap.get(j);
+
+            tempCity.setIndex(index);
+            city.setIndex(j);
+
+            this.minHeap.set(j, city);
+            this.minHeap.set(index, tempCity);
+            heapifyDown(city, j);
+        }
+    }
+
+    
+///////////////////////////////////////////////////////////////////////////////
+//                           DANGER ZONE                                     //
+//                everything below is used for grading                       //
+//                      please do not change :)                              //
+///////////////////////////////////////////////////////////////////////////////
+
+    public ArrayList<City> toArrayList() {
+        return minHeap;
+    }
+}
---
+++ Program2.java
@@ -0,0 +1,171 @@
+/*
+ * Name: Nikhil George
+ * EID: ng25762
+ */
+
+// Implement your algorithms here
+// Methods may be added to this file, but don't remove anything
+// Include this file in your final submission
+
+import java.util.ArrayList;
+
+public class Program2 implements ProgramTwoInterface {
+    
+    
+    // additional constructor fields may be added, but don't delete or modify anything already here
+
+
+    /**
+     * findMinimumRouteDistance(Problem problem)
+     *
+     *  @param problem - the problem to solve.
+     * 
+     *  @param problem  - contains the cities, start, dest, and heap.
+     * 
+     * @return the minimum distance possible to get from start to dest.
+     * Assume the given graph is always connected.
+     */
+    public int findMinimumRouteDistance(Problem problem) {
+        // System.out.println("We are here");
+
+        // Some code to get you started
+        City start = problem.getStart();
+        City dest = problem.getDest();
+        start.setMinDist(0);
+
+        HeapInterface heap = problem.getHeap();     // get the heap
+        heap.buildHeap(problem.getCities());        // build the heap
+
+        // TODO: implement this function
+        // System.out.println("source: "+start.getName());
+        // System.out.println("dest: "+dest.getName());
+        // System.out.println();
+        // System.out.println("Initial heap");
+        // for(City x: heap.toArrayList()){
+        //     // System.out.println(x.getName() +" "+ x.getIndex() +" "+ x.getMinDist());
+        //     for(int i = 0; i<x.getNeighbors().size(); i++){
+        //         City neighbor = x.getNeighbors().get(i);
+        //         int weight = x.getWeights().get(i);
+        //         // System.out.print(neighbor.getName() + "--" + weight + "; ");
+        //     }
+        //     // System.out.println();
+        // }
+
+        City min;
+        ArrayList<City> visited = new ArrayList<>(); 
+
+        while(heap.toArrayList().size()!=0){
+            // System.out.println("Heap------>");
+            // for(City x: heap.toArrayList()){
+            //     System.out.println(x.getName() +" "+ x.getIndex() +" "+ x.getMinDist());
+            // }
+            // System.out.println();
+            min = heap.extractMin();
+            // System.out.println("Heap after min extraction------>");
+            // for(City x: heap.toArrayList()){
+            //     System.out.println(x.getName() +" "+ x.getIndex() +" "+ x.getMinDist());
+            // }
+            // System.out.println();
+            // System.out.println("Extracted node: "+min.getName()+" mindist:"+min.getMinDist());
+            visited.add(min);
+            // for(City m : visited){
+            //     System.out.print(m.getName()+"----"+m.getMinDist()+" ");
+            // }
+            // System.out.println();
+            // for(City neighbor: min.getNeighbors()){
+            for(int i = 0; i<min.getNeighbors().size(); i++){
+                City neighbor = min.getNeighbors().get(i);
+                int weightFromMin = min.getWeights().get(i);
+                // System.out.println(i + " neighbor: "+ neighbor.getName()+ " weightfromMin: " +weightFromMin);
+                // System.out.println("neighbor mindist: "+ neighbor.getMinDist()+ " min mindist: " +min.getMinDist()+" weightfromMin: " +weightFromMin);
+                if(neighbor.getMinDist() > min.getMinDist()+weightFromMin){
+                    heap.changeKey(neighbor, min.getMinDist()+weightFromMin);
+                    // System.out.println();
+                }
+            }
+        }
+
+
+        return dest.getMinDist();
+    }
+
+    /**
+     * findMinimumLength()
+     *
+     * @return The minimum total optical line length required to connect (span) each city on the given graph.
+     * Assume the given graph is always connected.
+     */
+    public int findMinimumLength(Problem problem) {
+        // TODO: implement this function
+        // City start = problem.getStart();
+        City start = problem.getCities().get(0);
+        start.setMinDist(0);
+
+        HeapInterface heap = problem.getHeap();     // get the heap
+        heap.buildHeap(problem.getCities());        // build the heap
+
+        City min;
+        int length = 0;
+
+        while(heap.toArrayList().size()!=0){
+            // System.out.println("Heap------>");
+            // for(City x: heap.toArrayList()){
+            //     System.out.println(x.getName() +" "+ x.getIndex() +" "+ x.getMinDist());
+            // }
+            // System.out.println();
+            min = heap.extractMin();
+            length += min.getMinDist();
+            // System.out.println("current Length: "+ length);
+            // System.out.println("Heap after min extraction------>");
+            // for(City x: heap.toArrayList()){
+            //     System.out.println(x.getName() +" "+ x.getIndex() +" "+ x.getMinDist());
+            // }
+            // System.out.println();
+            // System.out.println("Extracted node: "+min.getName()+" mindist:"+min.getMinDist());
+            for(int i = 0; i<min.getNeighbors().size(); i++){
+                City neighbor = min.getNeighbors().get(i);
+                int weightFromMin = min.getWeights().get(i);
+                // System.out.println(i + " neighbor: "+ neighbor.getName()+ " weightfromMin: " +weightFromMin);
+                // System.out.println("neighbor mindist: "+ neighbor.getMinDist()+ " min mindist: " +min.getMinDist()+" weightfromMin: " +weightFromMin);
+                if(heap.toArrayList().contains(neighbor) && weightFromMin < neighbor.getMinDist()){
+                    heap.changeKey(neighbor, weightFromMin);
+                }
+            }
+
+        }
+        // System.out.println("Heap------>");
+        // for(City x: heap.toArrayList()){
+        //     System.out.println(x.getName() +" "+ x.getIndex() +" "+ x.getMinDist());
+        // }
+
+        return length;
+    }
+
+    //returns edges and weights in a string.
+    public String toString(Problem problem){ 
+        String o = "";
+        for (City v : problem.getCities()) {
+            boolean first = true;
+            o += "City ";
+            o += v.getName();
+            o += " has neighbors ";
+            ArrayList<City> ngbr = v.getNeighbors();
+            for (City n : ngbr) {
+                o += first ? n.getName() : ", " + n.getName();
+                first = false;
+            }
+            first = true;
+            o += " with distances ";
+            ArrayList<Integer> wght = v.getWeights();
+            for (Integer i : wght) {
+                o += first ? i : ", " + i;
+                first = false;
+            }
+            o += System.getProperty("line.separator");
+
+        }
+
+        return o;
+    }
+
+}
